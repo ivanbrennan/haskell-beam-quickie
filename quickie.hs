893cb482b35596791362eb61fcd42f8228ac4430
ivanbrennan <ivan.brennan@gmail.com>
2019-08-19 08:03:35 -0400

    part 2 wip

--- quickie.hs
+++ quickie.hs
@@ -1,18 +1,25 @@
 {-# LANGUAGE DeriveAnyClass       #-}
 {-# LANGUAGE DeriveGeneric        #-}
 {-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE ImpredicativeTypes   #-}
 {-# LANGUAGE OverloadedStrings    #-}
 {-# LANGUAGE StandaloneDeriving   #-}
 {-# LANGUAGE TypeApplications     #-}
 {-# LANGUAGE TypeFamilies         #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 
-import Database.Beam (Beamable, Columnar, Database, DatabaseSettings, Generic,
-                      Identity, PrimaryKey, Table, TableEntity, aggregate_, all_,
-                      as_, asc_, countAll_, defaultDbSettings, desc_, group_,
-                      insert, insertValues, liftIO, orderBy_, primaryKey,
-                      runInsert, runSelectReturningList, runSelectReturningOne,
-                      select)
+-- tableLenses, dbLenses
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
+
+import Database.Beam (Beamable, C, Columnar, Database, DatabaseSettings, Generic,
+                      Identity, LensFor(LensFor), PrimaryKey, Table, TableEntity,
+                      TableLens(TableLens), aggregate_, all_, as_, asc_,
+                      countAll_, dbLenses, defaultDbSettings, desc_,
+                      dbModification, fieldNamed, group_, insert, insertValues,
+                      liftIO, modifyTableFields, orderBy_, primaryKey, runInsert,
+                      runSelectReturningList, runSelectReturningOne, select,
+                      setEntityName, tableLenses, tableModification,
+                      withDbModification)
 import Database.Beam.Sqlite (runBeamSqliteDebug)
 import Database.SQLite.Simple (open)
 import Data.Text (Text)
@@ -36,38 +43,84 @@ instance Table UserT where
   primaryKey = UserId . _userEmail
 
 
+data AddressT f = Address
+  { _addressId    :: C f Int
+  , _addressLine1 :: C f Text
+  , _addressLine2 :: C f (Maybe Text)
+  , _addressCity  :: C f Text
+  , _addressState :: C f Text
+  , _addressZip   :: C f Text
+  , _addressForUser :: PrimaryKey UserT f
+  } deriving (Generic, Beamable)
+
+type Address = AddressT Identity
+
+deriving instance Show (PrimaryKey UserT Identity)
+deriving instance Show Address
+
+instance Table AddressT where
+  data PrimaryKey AddressT f = AddressId (C f Int) deriving (Generic, Beamable)
+  primaryKey = AddressId . _addressId
+
+
+User (LensFor userEmail)
+     (LensFor userFirstName)
+     (LensFor userLastName)
+     (LensFor userPassword) = tableLenses
+
+Address (LensFor addressId)
+        (LensFor addressLine1)
+        (LensFor addressLine2)
+        (LensFor addressCity)
+        (LensFor addressState)
+        (LensFor addressZip)
+        (UserId (LensFor addressForUserId)) = tableLenses
+
+ShoppingCartDb (TableLens shoppingCartUsers)
+               (TableLens shoppingCartUserAddresses) = dbLenses
+
+
 data ShoppingCartDb f = ShoppingCartDb
-  { _shoppingCartUsers :: f (TableEntity UserT)
+  { _shoppingCartUsers         :: f (TableEntity UserT)
+  , _shoppingCartUserAddresses :: f (TableEntity AddressT)
   } deriving (Generic, Database backend)
 
 shoppingCartDb :: DatabaseSettings backend ShoppingCartDb
-shoppingCartDb = defaultDbSettings
+shoppingCartDb =
+  defaultDbSettings `withDbModification` dbModification
+    { _shoppingCartUserAddresses =
+        ( setEntityName "addresses"
+        <> modifyTableFields tableModification
+             { _addressLine1 = fieldNamed "address1"
+             , _addressLine2 = "address2" -- OverloadedStrings
+             }
+        )
+    }
 
 
 main :: IO ()
 main = do
   conn <- open "shoppingcart.db"
 
+  let james = User "james@example.com"
+                   "James"
+                   "Smith"
+                   "b4cc344d25a2efe540adbf2678e2304c"
+
+      betty = User "betty@example.com"
+                   "Betty"
+                   "Jones"
+                   "82b054bd83ffad9b6cf8bdb98ce3cc2f"
+
+      sam = User "sam@example.com"
+                 "Sam"
+                 "Taylor"
+                 "332532dcfaa1cbf61e2a266bd723612c"
+
   runBeamSqliteDebug putStrLn conn $
     runInsert $
       insert (_shoppingCartUsers shoppingCartDb) $
-        insertValues
-          [ User
-              "james@example.com"
-              "James"
-              "Smith"
-              "b4cc344d25a2efe540adbf2678e2304c"
-          , User
-              "betty@example.com"
-              "Betty"
-              "Jones"
-              "82b054bd83ffad9b6cf8bdb98ce3cc2f"
-          , User
-              "sam@example.com"
-              "Sam"
-              "Taylor"
-              "332532dcfaa1cbf61e2a266bd723612c"
-          ]
+        insertValues [ james, betty, sam ]
 
   let allUsers = all_ (_shoppingCartUsers shoppingCartDb)
 
