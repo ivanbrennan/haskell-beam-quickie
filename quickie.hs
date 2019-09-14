427fce37d4d7ba87e6e502a3dd8a17a29871c3c5
ivanbrennan <ivan.brennan@gmail.com>
2019-09-14 08:43:35 -0400

    still not working

--- quickie.hs
+++ quickie.hs
@@ -12,8 +12,8 @@
 {-# OPTIONS_GHC -Wno-missing-signatures #-}
 
 import Database.Beam (Beamable, C, Columnar, Database, DatabaseSettings, Generic,
-                      Identity, LensFor(LensFor), PrimaryKey, Table, TableEntity,
-                      TableLens(TableLens), aggregate_, all_, as_, asc_,
+                      Identity, LensFor(LensFor), Nullable, PrimaryKey, Table,
+                      TableEntity, TableLens(TableLens), aggregate_, all_, as_, asc_,
                       countAll_, dbLenses, defaultDbSettings, desc_,
                       dbModification, fieldNamed, group_, insert, insertValues,
                       liftIO, modifyTableFields, orderBy_, primaryKey, runInsert,
@@ -23,6 +23,7 @@ import Database.Beam (Beamable, C, Columnar, Database, DatabaseSettings, Generic
 import Database.Beam.Sqlite (runBeamSqliteDebug)
 import Database.SQLite.Simple (open)
 import Data.Text (Text)
+import Data.Time (LocalTime)
 import Lens.Micro ((^.))
 
 
@@ -79,6 +80,64 @@ instance Table ProductT where
   primaryKey = ProductId . _productId
 
 
+deriving instance Show (PrimaryKey AddressT Identity)
+
+data OrderT f = Order
+  { _orderId            :: C f Int
+  , _orderDate          :: C f LocalTime
+  , _orderForUser       :: PrimaryKey UserT f
+  , _orderShipToAddress :: PrimaryKey AddressT f
+  , _orderShippingInfo  :: PrimaryKey ShippingInfoT (Nullable f)
+  } deriving (Generic, Beamable)
+
+type Order = OrderT Identity
+deriving instance Show Order
+
+instance Table OrderT where
+  data PrimaryKey OrderT f = OrderId (C f Int) deriving (Generic, Beamable)
+  primaryKey = OrderId . _orderId
+
+
+data ShippingCarrier
+  = USPS
+  | FedEx
+  | UPS
+  | DHL
+  deriving (Show, Read, Eq, Ord, Enum)
+
+data ShippingInfoT f = ShippingInfo
+  { _shippingInfoId             :: C f Int
+  , _shippingInfoCarrier        :: C f ShippingCarrier
+  , _shippingInfoTrackingNumber :: C f Text
+  } deriving (Generic, Beamable)
+
+type ShippingInfo = ShippingInfoT Identity
+deriving instance Show ShippingInfo
+
+instance Table ShippingInfoT where
+  data PrimaryKey ShippingInfoT f = ShippingInfoId (C f Int) deriving (Generic, Beamable)
+  primaryKey = ShippingInfoId . _shippingInfoId
+
+deriving instance Show (PrimaryKey ShippingInfoT (Nullable Identity))
+
+deriving instance Show (PrimaryKey OrderT Identity)
+deriving instance Show (PrimaryKey ProductT Identity)
+
+data LineItemT f = LineItem
+  { _lineItemInOrder    :: PrimaryKey OrderT f
+  , _lineItemForProduct :: PrimaryKey ProductT f
+  , _lineItemQuantity   :: C f Int
+  } deriving (Generic, Beamable)
+
+type LineItem = LineItemT Identity
+deriving instance Show LineItem
+
+instance Table LineItemT where
+  data PrimaryKey LineItemT f = LineItemId (PrimaryKey OrderT f) (PrimaryKey ProductT f)
+                                deriving (Generic, Beamable)
+  primaryKey = LineItemId <$> _lineItemInOrder <*> _lineItemForProduct
+
+
 User (LensFor userEmail)
      (LensFor userFirstName)
      (LensFor userLastName)
@@ -92,13 +151,28 @@ Address (LensFor addressId)
         (LensFor addressZip)
         (UserId (LensFor addressForUserId)) = tableLenses
 
+LineItem _ _ (LensFor lineItemQuantity) = tableLenses
+
+Product (LensFor productId)
+        (LensFor productTitle)
+        (LensFor productDescription)
+        (LensFor productPrice) = tableLenses
+
 ShoppingCartDb (TableLens shoppingCartUsers)
-               (TableLens shoppingCartUserAddresses) = dbLenses
+               (TableLens shoppingCartUserAddresses)
+               (TableLens shoppingCartProducts)
+               (TableLens shoppingCartOrders)
+               (TableLens shoppingCartShippingInfos)
+               (TableLens shoppingCartLineItems) = dbLenses
 
 
 data ShoppingCartDb f = ShoppingCartDb
   { _shoppingCartUsers         :: f (TableEntity UserT)
   , _shoppingCartUserAddresses :: f (TableEntity AddressT)
+  , _shoppingCartProducts      :: f (TableEntity ProductT)
+  , _shoppingCartOrders        :: f (TableEntity OrderT)
+  , _shoppingCartShippingInfos :: f (TableEntity ShippingInfoT)
+  , _shoppingCartLineItems     :: f (TableEntity LineItemT)
   } deriving (Generic, Database backend)
 
 shoppingCartDb :: DatabaseSettings backend ShoppingCartDb
@@ -111,6 +185,22 @@ shoppingCartDb =
              , _addressLine2 = "address2" -- OverloadedStrings
              }
         )
+    , _shoppingCartProducts = setEntityName "products"
+    , _shoppingCartOrders =
+        ( setEntityName "orders"
+        <> modifyTableFields tableModification
+             { _orderShippingInfo = ShippingInfoId "shipping_info__id"
+             }
+        )
+    , _shoppingCartShippingInfos =
+        ( setEntityName "shipping_info"
+        <> modifyTableFields tableModification
+             { _shippingInfoId = "id"
+             , _shippingInfoCarrier = "carrier"
+             , _shippingInfoTrackingNumber = "tracking_number"
+             }
+        )
+    , _shoppingCartLineItems = setEntityName "line_items"
     }
 
 
@@ -138,36 +228,43 @@ main = do
       insert (_shoppingCartUsers shoppingCartDb) $
         insertValues [ james, betty, sam ]
 
-  let addresses = [ Address
-                      default_
-                      (val_ "123 Little Street")
-                      (val_ Nothing)
-                      (val_ "Boston")
-                      (val_ "MA")
-                      (val_ "12345")
-                      (val_ (pk james))
-                  , Address
-                      default_
-                      (val_ "222 Main Street")
-                      (val_ (Just "Ste 1"))
-                      (val_ "Houston")
-                      (val_ "TX")
-                      (val_ "8888")
-                      (val_ (pk betty))
-                  , Address
-                      default_
-                      (val_ "9999 Residence Ave")
-                      (val_ Nothing)
-                      (val_ "Sugarland")
-                      (val_ "TX")
-                      (val_ "8989")
-                      (val_ (pk betty))
-                  ]
+  -- let addresses = [ Address
+  --                     default_
+  --                     (val_ "123 Little Street")
+  --                     (val_ Nothing)
+  --                     (val_ "Boston")
+  --                     (val_ "MA")
+  --                     (val_ "12345")
+  --                     (val_ (pk james))
+  --                 , Address
+  --                     default_
+  --                     (val_ "222 Main Street")
+  --                     (val_ (Just "Ste 1"))
+  --                     (val_ "Houston")
+  --                     (val_ "TX")
+  --                     (val_ "8888")
+  --                     (val_ (pk betty))
+  --                 , Address
+  --                     default_
+  --                     (val_ "9999 Residence Ave")
+  --                     (val_ Nothing)
+  --                     (val_ "Sugarland")
+  --                     (val_ "TX")
+  --                     (val_ "8989")
+  --                     (val_ (pk betty))
+  --                 ]
 
-  runBeamSqliteDebug putStrLn conn $
-    runInsert $
-      insert (_shoppingCartUserAddresses shoppingCartDb) $
-        insertExpressions addresses
+  -- runBeamSqliteDebug putStrLn conn $
+  --   runInsert $
+  --     insert (_shoppingCartUserAddresses shoppingCartDb) $
+  --       insertExpressions addresses
+
+  -- let products = [ Product default_ (val_ "Red Ball") (val_ "A bright red, very spherical ball") (val_ 1000)
+  --                , Product default_ (val_ "Math Textbook") (val_ "Contains a lot of important math theorems and formulae") (val_ 2500)
+  --                , Product default_ (val_ "Intro to Haskell") (val_ "Learn the best programming language in the world") (val_ 3000)
+  --                , Product default_ (val_ "Suitcase") "A hard durable suitcase" 15000
+  --                ]
+
 
   addresses' <- runBeamSqliteDebug putStrLn conn $
                 runSelectReturningList $
